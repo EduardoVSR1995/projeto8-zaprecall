3eeea371e3fc6ad889237c4db6aa35d9b6266ac1
EduardoVSR1995 <eduardvitor7@gmail.com>
2022-07-19 17:34:49 -0300

    fim montagem tela inicial

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
 export default function App(){
+    
+    
     return(
-        <div>                eeeeeeeeettttttttttt</div>
+        <div className="display-initial">
+            <div className="img">
+                <img src="./image/logo.png"/>
+            </div>
+        <div className="logo-name"> 
+            ZapRecall
+        </div>
+        <div onClick={()=>""} className="botton-initial">
+            Iniciar Recall!
+        </div>
+        </div>
     );
 }
